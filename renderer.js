d877f5ad2949905e4fa993331e8fa4bd98117b75
Matix <116652735+MatixGX@users.noreply.github.com>
2023-12-30 21:13:27 +0100

    Synced the whole thing up a little bit more :)

--- renderer.js
+++ renderer.js
@@ -6,42 +6,57 @@ const secondText = document.getElementById("s");
 const currentYear = new Date().getFullYear();
 const newYear = new Date(currentYear + 1, 0, 1, 0, 0, 0, 0);
 
-console.log(newYear);
 function padWithLeadingZeros(num, totalLength) {
   return String(num).padStart(totalLength, "0");
 }
 
-function refreshDisplay() {
-  const now = Date.now();
+fetch("http://worldtimeapi.org/api/timezone/Europe/Warsaw", {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+}).then(async (response) => {
+  response = await response.json();
+  const startTime = new Date(response.datetime);
+  const myOffset = Date.now() - startTime;
 
-  diff = newYear - now;
+  console.log(myOffset);
 
-  const d = Math.floor((diff / 1000 / 60 / 60 / 24) % 365);
-  const h = Math.floor((diff / 1000 / 60 / 60) % 24);
-  const m = Math.floor((diff / 1000 / 60) % 60);
-  const s = Math.floor((diff / 1000) % 60);
+  async function refreshDisplay() {
+    const now = Date.now() - myOffset;
 
-  dayText.innerHTML = d;
-  hourText.innerHTML = padWithLeadingZeros(h, 2);
-  minuteText.innerHTML = padWithLeadingZeros(m, 2);
-  secondText.innerHTML = padWithLeadingZeros(s, 2);
+    diff = newYear - now;
 
-  if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
-    const body = document.body;
-    body.style.background = "rgb(0, 0, 0)";
-    document.title = `WELCOME ${newYear.getFullYear()}!`;
-    body.innerHTML = `<img class="fireworks" src="https://i.pinimg.com/originals/ba/ce/57/bace57c5e51b79fa303026d754fef8b5.gif"><h1 class="title" id="title">IT'S ${newYear.getFullYear()}!!! WOHO!!!</h1><img class="fireworks" src="https://i.pinimg.com/originals/ba/ce/57/bace57c5e51b79fa303026d754fef8b5.gif">`;
-    var colors = ["red", "yellow", "white"];
-    var i = 0;
-    setInterval(() => {
-      document.getElementById("title").style.color = colors[i];
-      i += 1;
-      if (i == colors.length) {
-        i = 0;
-      }
-    }, 1000);
+    const d = Math.floor((diff / 1000 / 60 / 60 / 24) % 365);
+    const h = Math.floor((diff / 1000 / 60 / 60) % 24);
+    const m = Math.floor((diff / 1000 / 60) % 60);
+    const s = Math.floor((diff / 1000) % 60);
+
+    dayText.innerHTML = d;
+    hourText.innerHTML = padWithLeadingZeros(h, 2);
+    minuteText.innerHTML = padWithLeadingZeros(m, 2);
+    secondText.innerHTML = padWithLeadingZeros(s, 2);
+
+    if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+      const body = document.body;
+      body.style.background = "rgb(0, 0, 0)";
+      document.title = `WELCOME ${newYear.getFullYear()}!`;
+      body.innerHTML = `<img class="fireworks" src="https://i.pinimg.com/originals/ba/ce/57/bace57c5e51b79fa303026d754fef8b5.gif"><h1 class="title" id="title">IT'S ${newYear.getFullYear()}!!! WOHO!!!</h1><img class="fireworks" src="https://i.pinimg.com/originals/ba/ce/57/bace57c5e51b79fa303026d754fef8b5.gif">`;
+      var colors = ["red", "yellow", "white"];
+      var i = 0;
+      setInterval(() => {
+        document.getElementById("title").style.color = colors[i];
+        i += 1;
+        if (i == colors.length) {
+          i = 0;
+        }
+      }, 500);
+    }
   }
-}
 
-refreshDisplay();
-setInterval(refreshDisplay, 1000);
+  refreshDisplay();
+  var off = myOffset;
+  setTimeout(() => {
+    setInterval(refreshDisplay, 100);
+  }, myOffset);
+});
